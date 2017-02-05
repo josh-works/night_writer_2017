f8bf7c74acc05a8f61e99b6e4252b8f137969e87
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-05 12:12:25 -0700

    remove comments

--- lib/text_to_braille.rb
+++ lib/text_to_braille.rb
@@ -1,8 +1,6 @@
 require 'pry'
 class TextToBraille
   attr_accessor :line0, :line1, :line2
-  # 0 => raised dot
-  # . => unraised space
   BRAILLEALPHABET = {
     "a" => ["0.","..",".."], "b" => ["0.","0.",".."], "c" => ["00","..",".."], "d" => ["00",".0",".."],
     "e" => ["0.",".0",".."], "f" => ["00","0.",".."], "g" => ["00","00",".."], "h" => ["0.","00",".."],
@@ -49,31 +47,4 @@ class TextToBraille
       file << line2.join() + "\n"
     end
   end
-
 end
-
-# def encrypt_file(filename, rotation)
-#   # create file handle to the input file
-#   file = File.open(filename, "r")
-#
-#   # read text of input file
-#   message_to_encrypt = file.read
-#   encrypted_message = encrypt(message_to_encrypt, rotation)
-#   output = File.open("#{filename}.#{rotation}.encrypted", "w")
-#   output.write(encrypted_message)
-#   output.close
-#   puts "#{encrypted_message} | written to new file"
-# end
-#
-# def decrypt_file(filename, rotation)
-#     file = File.open(filename, "r")
-#     message_to_decrypt = file.read
-#     decrypted_message = decrypt(message_to_decrypt, rotation)
-#     output = File.open("#{filename.gsub("encrypted", "decrypted")}", "w")
-#     output.write(decrypted_message)
-#     output.close
-# end
-#
-# letter = TextToBraille.new
-# braille = letter.print_letter("a")
-# letter.combine_letters(braille)
