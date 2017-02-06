e8c85d3e73389957437e229fbbfe5c650cf97639
Seth <seth19483@gmail.com>
2017-02-06 12:54:09 -0700

    add psuedocode

--- lib/night_read.rb
+++ lib/night_read.rb
@@ -1,3 +1,4 @@
+require_relative'letters'
 class NightRead
 	attr_reader :read
 	def initialize
@@ -10,10 +11,28 @@ class NightRead
 	end
 
 	def file_array
-		file_arr = @read.split("\n")
-		file_arr.map do |line|
-			line.split(/../)
+		# until file_arr.count == 3
+		file_arr = @read.split("\n")	
+		if file_arr.count <= 3
+			require'pry';binding.pry
+			file_arr.map do |line|
+				line.scan(/../)
+			end
 		end
+	end
+
+	def psuedocode
+		#each with index
+		#new_array = []
+		#file_arr.each_with_index do |line, index|
+		#		new_arr_#{index} = []
+		#		new_array << new_arr_#{index} << line
+		#end
+		#word = []
+		#new_array.each do |letter_arr|
+		#		letter = Letters.new(letter_arr)
+		#		word << letter
+		#end
 	end
 end
 
--- test/letters_test.rb
+++ test/letters_test.rb
@@ -14,6 +14,15 @@ class LettersTest < Minitest::Test
 		letter_4 = Letters.new(["0.", "0.", "0."])
 		letter_5 = Letters.new(["0.", ".0", "0."])
 		word = letter_1.convert + letter_2.convert + letter_3.convert + letter_4.convert + letter_5.convert
+		
+		word_arr = Array.new
+		word_arr << letter_1.convert
+		word_arr << letter_2.convert
+		word_arr << letter_3.convert
+		word_arr << letter_4.convert
+		word_arr << letter_5.convert
+
 		assert_equal "hello", word
+		assert_equal "hello", word_arr.join
 	end
 end
