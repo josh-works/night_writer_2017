a7ff5bcda65cd87b72dfcd56c9120bd8af5150ad
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-07 08:44:11 -0700

    add minor refactor

--- lib/text_to_braille.rb
+++ lib/text_to_braille.rb
@@ -1,33 +1,23 @@
 require 'pry'
-require './FileReader'
+require './braille.rb'
 
 
 class TextToBraille
-  attr_accessor :line0, :line1, :line2, :filename, :reader, :char_count
-  BRAILLEALPHABET = {
-    "a" => ["0.","..",".."], "b" => ["0.","0.",".."], "c" => ["00","..",".."], "d" => ["00",".0",".."],
-    "e" => ["0.",".0",".."], "f" => ["00","0.",".."], "g" => ["00","00",".."], "h" => ["0.","00",".."],
-    "i" => [".0","0.",".."], "j" => [".0","00",".."], "k" => ["0.","..","0."], "l" => ["0.","0.","0."],
-    "m" => ["00","..","0."], "n" => ["00",".0","0."], "o" => ["0.",".0","0."], "p" => ["00","0.","0."],
-    "q" => ["00","00","0."], "r" => ["0.","00","0."], "s" => [".0","0.","0."], "t" => [".0","00","0."],
-    "u" => ["0.","..","00"], "v" => ["0.","0.","00"], "w" => [".0","00",".0"], "x" => ["00","..","00"],
-    "y" => ["00",".0","00"], "z" => ["0.",".0","00"], "!" => ["..","00","0."], "'" => ["..","..","0."],
-    "," => ["..","0.",".."], "-" => ["..","..","00"], "." => ["..","00",".0"], "?" => ["..","0.","00"],
-    "capitals" => ["..", "..", ".0"], "numbers" => [".0", ".0", "00"], " " => ["..","..",".."]
-   }
+  attr_accessor :line0, :line1, :line2, :filename, :char_count, :text
 
   def initialize
     @line0 = []
     @line1 = []
     @line2 = []
-    @filename = nil
-    @reader = FileReader.new
-    @char_count = 0
-    read_from_file
+    @filename = ARGV[0]
+    @text = File.read(filename)
+    @char_count = text.length
+    puts "... retrieved '#{text}' from #{filename} \n"
+    print_phrase(text)
   end
 
   def print_phrase(phrase)
-    phrase.split('').map do |letter|
+    phrase.chomp.split('').map do |letter|
       letter_to_braille(letter)
     end
     puts "#{line0.join()}" +  "\n#{line1.join()}" + "\n#{line2.join()}"
@@ -35,24 +25,16 @@ class TextToBraille
   end
 
   def letter_to_braille(letter)
-    hash = BRAILLEALPHABET[letter]
+    binding.pry
+    hash = Braille::BRAILLEALPHABET[letter]
     line0 << hash[0]
     line1 << hash[1]
     line2 << hash[2]
   end
 
-  def read_from_file
-    @filename = ARGV[0]
-    text = reader.read
-    @char_count = text.length
-    puts "... retrieved '#{text}' from #{filename} \n"
-
-    print_phrase(text)
-  end
-
   def print_to_file
     if ARGV[1].nil?
-      output_name = filename.slice(0..-5) + ".braille.txt"
+      output_name = filename.slice(0..-5) + "_braille.txt"
     else
       output_name = ARGV[1]
     end
