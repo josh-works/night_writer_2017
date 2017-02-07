618573c67a556c756a05244000e5404c39d72e2d
Seth <seth19483@gmail.com>
2017-02-07 11:30:20 -0700

    add zip_every_three method

--- lib/night_read.rb
+++ lib/night_read.rb
@@ -8,17 +8,8 @@ class NightRead
 		@read = File.read("./test_file.txt").split("\n")
 		@full_text = []
 		@scan_arr = []
-		@conterted_strings_by_line = []
 	end
-
-	def split_by_three
-		num = @read.count / 3
-		num.times do 
-			@full_text << @read.shift(3)
-		end
-		@full_text		
-	end
-
+	
 	def split_into_pairs
 		@read.each do |letter|
 				@scan_arr << letter.scan(/../)
@@ -26,44 +17,26 @@ class NightRead
 		@scan_arr
 	end
 
-	def zip_array
-		zip_arr = []
-		letters = []
-		split_into_pairs.each do |array|
-			line1 = array[0].split(" ")
-			line2 = array[1].split(" ")
-			line3 = array[2].split(" ")
-			# binding.pry
-			zip_arr << line1.zip(line2, line3)
+	def zip_every_three
+		split_into_pairs
+		zipped = []
+		translated = []
+		line1 = @scan_arr[0]
+		line2 = @scan_arr[1]
+		line3 = @scan_arr[2]
+			zipped << line1.zip(line2, line3)
+		zipped[0].each do |letter_arr|
+			translate = Letters.new(letter_arr)
+			translated << translate.convert
 		end
-			zip_arr.each do |letter|
-				translator = Letters.new(letter)
-				letters << translator.convert
-			# binding.pry
-		@converted_strings_by_line << letters.join
-			end
-		# binding.pry
-		@converted_strings_by_line
+		@scan_arr.shift(3)
+		translated.join
 	end
 
 end
 
-		#line1.zip(line2, line3) method
-		#each with index
-		#new_array = []
-		#file_arr.each_with_index do |line, index|
-		#		new_arr_#{index} = []
-		#		new_array << new_arr_#{index} << line
-		#end
-		#word = []
-		#new_array.each do |letter_arr|
-		#		letter = Letters.new(letter_arr)
-		#		word << letter
 
-NightRead.new.split_by_three
-NightRead.new.split_into_pairs
-NightRead.new.zip_array
-# split by every 2 characters .split(/../)
-# seperate by each line (and every 3 lines)
-# if index is same << into an array together
-# set specific arrays == the arrays it creates
+# NightRead.new.split_by_three
+# NightRead.new.split_into_pairs
+# NightRead.new.zip_array
+NightRead.new.zip_every_three
--- test/night_read_test.rb
+++ test/night_read_test.rb
@@ -9,15 +9,7 @@ class NightReadTest < Minitest::Test
 	def test_turns_into_an_array
 		assert_equal Array, @braille_text.read.class
 	end
-
-	def	test_break_into_smaller_arrays
-		new_arr = Array.new
-
-		new_arr << @braille_text.read
-		assert_equal 1, new_arr.length
-		@braille_text.split_by_three
-	end
-
+	
 	def test_can_split_by_two_characters
 		pair = @braille_text.split_into_pairs
 		
@@ -29,7 +21,11 @@ class NightReadTest < Minitest::Test
 		assert_equal 2, pair[0][5].length
 	end
 
-	def test_line_one_through_three_outputs_hello_world_from_test_file_txt
-		assert_equal "hello world", @braille_text.zip_array
+	def test_can_zip_can_read_every_three_lines_in_file
+		assert_equal "hello world", @braille_text.zip_every_three
+		assert_equal "world", @braille_text.zip_every_three
+		assert_equal "hello world", @braille_text.zip_every_three
+		assert_equal "hello world", @braille_text.zip_every_three
+		assert_equal "hello world", @braille_text.zip_every_three
 	end
 end
