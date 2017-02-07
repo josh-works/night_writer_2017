87f98b6a9987e71b954767eedfb4f63fdc297307
Seth <seth19483@gmail.com>
2017-02-07 09:51:03 -0700

    Add zip array

--- test/night_read_test.rb
+++ test/night_read_test.rb
@@ -15,6 +15,21 @@ class NightReadTest < Minitest::Test
 
 		new_arr << @braille_text.read
 		assert_equal 1, new_arr.length
-		@braille_text.split_into_letter_arrays
+		@braille_text.split_by_three
+	end
+
+	def test_can_split_by_two_characters
+		pair = @braille_text.split_into_pairs
+		
+		assert_equal 2, pair[0][0].length
+		assert_equal 2, pair[0][1].length
+		assert_equal 2, pair[0][2].length
+		assert_equal 2, pair[0][3].length
+		assert_equal 2, pair[0][4].length
+		assert_equal 2, pair[0][5].length
+	end
+
+	def test_line_one_through_three_outputs_hello_world_from_test_file_txt
+		assert_equal "hello world", @braille_text.zip_array
 	end
 end
